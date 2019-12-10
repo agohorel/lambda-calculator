a0036f226955ebfdbafb6a21340f8d64b4037fc8
agohorel <gohorelalex@gmail.com>
2019-12-10 12:03:08 -0500

    add some styling to buttons

--- src/components/Styles/StyledComponents.js
+++ src/components/Styles/StyledComponents.js
@@ -4,6 +4,13 @@ import { colors } from "./colors";
 const Button = styled.button`
   background-color: ${props =>
     props.operator ? colors.op : props.special ? colors.special : colors.num};
+  border: none;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  font-size: 32px;
+  color: white;
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.5);
 `;
 
 export { Button };
